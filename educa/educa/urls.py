a1eb6ee8240937cd3907801c5d75366b8d33c942
Ania Nieżychowska <aniamaciejewska320@gmail.com>
2022-09-18 17:41:04 +0200

    create simple api

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -20,17 +20,17 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
 
-
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
-          name='login'),
+         name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(),
-          name='logout'),
+         name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api/', include('courses.api.urls', namespace='api')),
 ]
 
 if settings.DEBUG:
